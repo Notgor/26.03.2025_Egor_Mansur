2da6f416341c0e505c1e81ee756d912654583eb8
Egor <egorkor88900@gmail.com>
2025-03-27 08:19:26 +0500

    bla bla bla

---
+++ Hometask_Egor_Mansur_26/Hometask_Egor_Mansur_26/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_Egor_Mansur_26
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine($"{Name} издаёт неопределённый звук.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} говорит: \"Гав!\"");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} говорит: \"Мяу!\"");
+        }
+    }
+
+    // Демонстрация полиморфизма
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal>
+        {
+            new Dog { Name = "Бобик", Age = 3 },
+            new Cat { Name = "Мурка", Age = 2 }
+        };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Name} ({animal.Age} года) говорит:");
+                animal.Speak();
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Hometask_Egor_Mansur_26/Hometask_Egor_Mansur_26/IPrintable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_Egor_Mansur_26
+{
+    public interface IPrintable
+    {
+        string Print();
+    }
+
+    public class Document : IPrintable
+    {
+        public string Content { get; set; }
+
+        public string Print()
+        {
+            return $"Document Content: {Content}";
+        }
+    }
+
+    public class Report : IPrintable
+    {
+        public string Title { get; set; }
+
+        public string Print()
+        {
+            return $"Report Title: {Title}";
+        }
+}
+
+// Демонстрация универсальности интерфейса
+var printableList = new List<IPrintable>
+{
+    new Document { Content = "Project Plan" },
+    new Report { Title = "Annual Report" }
+};
+
+foreach (var printable in printableList)
+{
+    Console.WriteLine(printable.Print());
+}
+
+}
